811a373790a2ced0fd1d5105f71dd143c7cd9eb7
Congrak <rodricelis@gmail.com>
2023-02-04 16:11:28 +0000

    Backend Working' git push origin main

--- src/api/models.py
+++ src/api/models.py
@@ -6,12 +6,31 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    username = db.Column(db.String(80), unique=True, nullable=True)
+    password = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(120), unique=False, nullable=True)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
+    country = db.Column(db.String(120), unique=False, nullable=True)
+    state = db.Column(db.String(120), unique=False, nullable=True)
+    city = db.Column(db.String(120), unique=False, nullable=True)
+    address = db.Column(db.String(30), unique=False, nullable=True)
+    zipcode = db.Column(db.String(30), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.id}>'
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "address": self.address,
+            "state": self.state,
+            "country": self.country,
+            "city": self.city,
+            "zipcode": self.zipcode,
             # do not serialize the password, its a security breach
         }
